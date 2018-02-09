c31c1b690ff1c9b2855065342e3c9260e7221a82
taniacao12 <tcaosu@stuy.edu>
2018-02-09 09:05:37 -0500

    Update MFDriver.java

--- MFDriver.java
+++ MFDriver.java
@@ -1,48 +1,28 @@
 public class MFDriver {
+    public static void main (String[] args) {
+	int numMatrices = 100;
+	int size = 100;
+	long totalTime = 0;
 
-  // Populate Method
-  public static void populate (int[][] array){
-    int count = 1;
-    for (int i = 0; i < array.length; i++){
-      for (int j = 0; j < array.length; j++){
-        array[i][j] = count;
-        count += 1;
-      }
-    }
-  }
-
-  public static void main (String[] args) {
-
-    System.out.println();
-
-    int numMatrices = 100;
-
-		int x = 100;
-		long totalTime = 0;
-
-		for (int n = 0; n < numMatrices; n++) { // 100 Matrices
-
-			long averageTime = 0;
-			int[][] test = new int[x][x];
-			MatrixFinder.populate(test);
-
-			for (int i = 0; i < 50; i++) { // 50 Trials
-
-				if (i != 0) {
-					long startTime = System.nanoTime();
-					MatrixFinder.locate(test, test[test.length - 1][0]);
-					long endTime = System.nanoTime();
-					averageTime += (endTime - startTime);
-				}
-
-			}
-
-			System.out.println(x + ", " + averageTime / 49 + "\n");
-			x += 100;
-			totalTime += (averageTime / 49);
+	for (int n = 0; n < numMatrices; n++) { // 100 Matrices
+	    long averageTime = 0;
+	    int[][] test = new int[size][size];
+	    MatrixFinder.populate(test);
+	    for (int i = 0; i <= 50; i++) { // 50 Trials
+	        if (i != 0) {
+		    long startTime = System.nanoTime();
+		    MatrixFinder.locate(test, test[test.length - 1][0]);
+		    long endTime = System.nanoTime();
+		    averageTime += (endTime - startTime);
 		}
 
-		System.out.println("Overall Average: " + totalTime / numMatrices);
+	    }
 
-  }
+	    System.out.println(size + "--> " + averageTime / 50 + "\n");
+	    totalTime += (averageTime / 50);
+	    size += 100;
+	}
+	
+	System.out.println("Overall Average: " + totalTime / numMatrices);
+    }
 }
