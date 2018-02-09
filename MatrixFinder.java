7024b94eabcea481c40ed2269d777f0b56489c89
taniacao12 <tcaosu@stuy.edu>
2018-02-09 08:56:45 -0500

    Update MatrixFinder.java

--- MatrixFinder.java
+++ MatrixFinder.java
@@ -11,48 +11,44 @@
 * As long as the row number and the column number are within the appropriate index
 * compare the current position to the target number:
 *    - If current number > target, you move down to the next number (aka row++;)
-*    - Otherwise current number < target, you move left to the next number (aka col--;)
-*    - If it isn't > or <, then the current number must be equal to target. Return the current index (row, col)
+*    - Otherwise current number < target, you move left to the next number
+*      (aka col--;)
+*    - If it isn't > or <, then the current number must be equal to target. Return
+*      the current index (row, col)
 * If at any point the index for row or column gets out of the range of the matrix,
-* the while loop ends and (-1,-1) is returned, meaning that the target is not in the matrix.
+* the while loop ends and (-1,-1) is returned, meaning that the target is not in 
+* the matrix.
 ****************************** */
 
 public class MatrixFinder {
 
-	// Locate Method
-	public static String locate (int[][] x, int target) {
+    // Locate Method
+    public static String locate (int[][] x, int target) {
+	int row = 0;
+	int col = x[0].length - 1;
 
-		int row = 0;
-		int col = x[0].length - 1;
-
-		while (col >= 0 && row < x.length) {
-			if (target > x[row][col]) {
-				row ++;
-			}
-			else if (target < x[row][col]) {
-				col --;
-			}
-			else {
-				return target + " found at: (" + row + "," + col + ")";
-			}
-		}
-		return target + " found at: (-1,-1)";
+	while (col >= 0 && row < x.length) {
+	    if (target > x[row][col]) {
+		row ++;
+	    }
+	    else if (target < x[row][col]) {
+		col --;
+	    }
+	    else {
+		return target + " found at: (" + row + "," + col + ")";
+	    }
 	}
+	return target + " found at: (-1,-1)";
+    }
 
-	// Populate Method
-	public static void populate (int[][] array){
-		int count = 1;
-		for (int i = 0; i < array.length; i++){
-			for (int j = 0; j < array.length; j++){
-				array[i][j] = count;
-				count += 1;
-			}
-		}
+    // Populate Method
+    public static void populate (int[][] array){
+	int count = 1;
+	for (int i = 0; i < array.length; i++){
+	    for (int j = 0; j < array.length; j++){
+		array[i][j] = count;
+		count += 1;
+	    }
 	}
-
-	// Main Method
-	public static void main (String[] args) {
-
-	}
-
+    }
 }
