0ed398b4ed1e82d594e3657cda1548fccaa551ba
Tania Cao <tcaosu@stuy.edu>
2018-02-08 19:15:39 -0500

    Update MatrixFinder.java

--- MatrixFinder.java
+++ MatrixFinder.java
@@ -8,56 +8,57 @@
  * Given a matrix, x, of dimensions n*n and the target
  * Begin at the starting position, (0,n-1)
  * Enter while loop
- * As long as the row number and the column number are within the appropriate index
- * compare the current position to the target number:
- *    - If current number > target, you move down to the next number (aka row++;)
- *    - Otherwise current number < target, you move left to the next number (aka col--;)
- *    - If it isn't > or <, then the current number must be equal to target. Return the current index (row, col)
- * If at any point the index for row or column gets out of the range of the matrix,
- * the while loop ends and (-1,-1) is returned, meaning that the target is not in the matrix.
-****************************** */
+ * As long as the row number and the column number are within the 
+ * appropriate index, compare the current position to the target number:
+ *    - If current number > target, you move down to the next number
+ *      (aka row++;)
+ *    - Otherwise current number < target, you move left to the next number 
+ *      (aka col--;)
+ *    - If it isn't > or <, then the current number must be equal to target.
+ *      Return the current index (row, col)
+ * If at any point the index for row or column gets out of the range of the
+ * matrix, the while loop ends and (-1,-1) is returned, meaning that the
+ * target is not in the matrix.
+ ****************************** */
 
 public class MatrixFinder {
-
-	// Locate Method
-	public static String locate (int[][] x, int target) {
-
-		int row = 0;
-		int col = x[0].length - 1;
-
-		while (col >= 0 && row < x.length) {
-			if (target > x[row][col]) {
-				row ++;
-			}
-			else if (target < x[row][col]) {
-				col --;
-			}
-			else {
-				return target + " found at: (" + row + "," + col + ")";
-			}
-		}
-		return target + " found at: (-1,-1)";
+    int[][] matrix;
+    
+    // overwrite retString to display the matrix
+    public static String retString() {
+	String s = "";
+	for (int r = 0; r < matrix.length; r++) {
+	    for (int c = 0; j < matrix[0].length; c++) {
+		s += matrix[r][c] + " ";
+	    }
 	}
+	return s;
+    }
 
-	// Main Method
-	public static void main (String[] args) {
-
-		System.out.println();
-
-		int[][] test = { {1,2,5} , {3,4,6} , {7,8,9} };
-
-		for (int i = 0; i < test.length; i++) {
-			for (int j = 0; j < test[0].length; j++) {
-				System.out.print(test[i][j] + " ");
-			}
-			System.out.println();
-		}
-
-		System.out.println();
-		System.out.println(locate(test, 5));
-		System.out.println(locate(test, 7));
-		System.out.println(locate(test, 4));
-		System.out.println(locate(test, 42));
+    // locate() goes through the given array[][] (matrix) and searches for
+    // the target. If found, it will return the indexes of the target. If
+    // not, it returns (-1,-1).
+    public static String locate (int[][] x,int target) {
+	int row = 0;
+	int col = x[0].length - 1;
+	while (col >= 0 && row < x.length) {
+	    if (target > x[row][col])
+		row ++;
+	    else if (target < x[row][col])
+		col --;
+	    else
+		return target + " found at: (" + row + "," + col + ")";
 	}
+	return target + " found at: (-1,-1)";
+    }
 
+    // should we put the execution time function here?
+    // eTime() runs the locate function on a given array[][] (matrix) and
+    // return the amount of time needed to execute the function.
+    public static long eTime(int[][] x) {
+	long start = System.nanoTime();
+	locate(x,x[x.length][x[0].length]);
+	long end = System.nanoTime();
+	return start - end;
+    }
 }
